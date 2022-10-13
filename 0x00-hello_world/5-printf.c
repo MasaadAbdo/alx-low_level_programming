5a4eac2c97b1336ae8eb4ae1d59ee28056e64d3d
masaadabdo <mssaadabdo@gmil.com>
2022-10-13 14:47:05 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
+*main - Entry point
 *Return: Always 0 (Success)
 */
 int main(void)
@@ -8,4 +8,3 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
- 
