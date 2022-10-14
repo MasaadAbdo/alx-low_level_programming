5f4e16a19ed638daa42cc49e9107257130460690
masaadabdo <mssaadabdo@gmil.com>
2022-10-14 15:46:58 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,16 @@ int main(void)
 int num;
 for (num = '0'; num <= '9'; num++)
 {
-putchar(num);	
+putchar(num);
 if (num == '9')
 {
 break;
-}										else
+}
+else
 {
 putchar(',');
-putchar(' ');									}
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
