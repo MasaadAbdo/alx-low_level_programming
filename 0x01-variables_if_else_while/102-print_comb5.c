e6dca8c3c18a5226e0c61037d8b2c3e81525f2ba
masaadabdo <mssaadabdo@gmil.com>
2022-10-14 17:40:30 -0700

    gg

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
 *main-entry point
+*
 *Description: prints all possible combinations of two two-digit numbers
-* Return:0 success, non zero fail
+*
+*Return:0 success, non zero fail
 */
 int main(void)
 {
@@ -13,9 +16,9 @@ for (j = i + 1; j <= 99; j++)
 {
 putchar('0' + i / 10);
 putchar('0' + i % 10);
-putchar(' '); 
+putchar(' ');
 putchar('0' + j / 10);
-putchar('0' + j %10); 
+putchar('0' + j % 10);
 if (i == 98 && j == 99)
 break;
 putchar(',');
