38bfc46c4bb96e1de212bedbb5607869622c85b7
masaadabdo <mssaadabdo@gmil.com>
2022-10-18 16:34:31 -0700

    n

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,10 @@
 #include "main.h"
 /**
- *  * print_sign - prints the sign of a num
- *  ber
- *   * @n: the number of the sign to be prin
- *   ted
- *    * Return: 1 and prints + if n is greate
- *    r than zero
- *     *0 and prints 0 if n is zero
- *      *-1 and prints - if n is less than zero
+* print_sign - prints the sign of a num ber
+* @n: the number of the sign to be printed
+* Return: 1 and prints + if n is greater than zero
+ *0 and prints 0 if n is zero
+*-1 and prints - if n is less than zero
 */
 int print_sign(int n)
 {
