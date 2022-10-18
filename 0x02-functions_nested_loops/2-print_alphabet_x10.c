84d5e4427f9ed4bedb7c53dad6c3eb3deccc0e4d
masaadabdo <mssaadabdo@gmil.com>
2022-10-18 13:35:35 -0700

    d

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *  *print_alphabet_x10 - print 10 times th
- *  e alphabet , in lowercase
+*print_alphabet_x10 - print 10 times th
+e alphabet , in lowercase
 */
 void print_alphabet_x10(void)
 {
@@ -12,4 +12,4 @@ while (count++ <= 9)
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
-}
+}}
