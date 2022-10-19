f2133620cd30e395c8c8c632aa961613c6ff84c4
masaadabdo <mssaadabdo@gmil.com>
2022-10-18 18:20:09 -0700

    f

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- *  * print_last_digit - prints the last di
- *  git
- *   * @n: the number
+ *  * print_last_digit -> prints the last digit
+ * @n: the pased args
  *    * Return: the last digit
  */
 int print_last_digit(int n)
 {
-int x;;
+int x;
 if (n < 0)
 n = -n;
 x = n % 10;
