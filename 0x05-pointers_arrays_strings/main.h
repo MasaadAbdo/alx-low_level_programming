fdae523f88d4d067fc742a0b6be7d539863af2eb
masaadabdo <mssaadabdo@gmil.com>
2022-10-24 16:09:55 -0700

    s

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 #include <stdio.h>
 /**
- * * void prototypes()
- * *char prototypes()
- * * int prototypes()
- * */
+*void prototypes()
+*char prototypes()
+*int prototypes()
+*/
 void reset_to_98(int *n);
 void _puts(char *str);
 void print_rev(char *s);
