1583eb8c1cc407ddd26935e9f7c66dfd25dedcb8
masaadabdo <mssaadabdo@gmil.com>
2022-10-31 08:44:40 -0700

    r

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,9 +6,8 @@
  * @dest: A pointer to the memory area to copy @src into.
  * @src: The source buffer to copy characters from.
  * @n: The number of bytes to copy from @src.
- *
  * Return: A pointer to the destination buffer @dest.
- */
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
