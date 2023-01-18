bc8d3672a25ce39f3e1cc36bd28e49a3e4715be9
masaadabdo <mssaadabdo@gmil.com>
2023-01-18 03:18:59 -0800

    ds

--- 0x18-dynamic_libraries/0-memset.c
+++ 0x18-dynamic_libraries/0-memset.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
-*_memset - The _memset() function fills
-* the first n bytes of the memory area
-* pointed to by s with the constant byte b
-*@s:target
-*@b: constant byte
-*@n:number of byte
-*Return: returns new value of target
-*/
-
+ * _memset - fills the memory with the constant byte
+ * @n : first n byte
+ * @s: point a byte
+ * @b: constant byte
+ *
+ * Return: apointer to the memory
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		s[n - 1] = b;
-		n--;
+		*(s + i) = b;
 	}
 	return (s);
 }
